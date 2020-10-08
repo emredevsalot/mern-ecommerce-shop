3d4f284a7b3b780fb8fa8d643ffe7cea2a0a4e93
Emre Ergül <eemreergul@gmail.com>
2020-10-08 17:15:56 +0300

    Added quantity for single products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
-import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-import { Container } from 'react-bootstrap'
-import Footer from './components/Footer'
-import Header from './components/Header'
-import HomeScreen from './screens/HomeScreen'
-import ProductScreen from './screens/ProductScreen'
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+import CartScreen from './screens/CartScreen';
 
 const App = () => {
   return (
@@ -15,11 +16,12 @@ const App = () => {
           <Route path='/' component={HomeScreen} exact />
 
           <Route path='/product/:id' component={ProductScreen} />
+          <Route path='/cart/:id?' component={CartScreen} />
         </Container>
       </main>
       <Footer />
     </Router>
-  )
-}
+  );
+};
 
-export default App
+export default App;
