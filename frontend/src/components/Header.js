ebd31455aaa57efa4acbc0ed9a9971b28d075598
Emre Ergül <eemreergul@gmail.com>
2020-10-21 17:25:57 +0300

    Admin User List

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,18 +1,17 @@
-import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap'
-import { LinkContainer } from 'react-router-bootstrap'
-import { logout } from '../actions/userActions'
-
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+import { logout } from '../actions/userActions';
 
 const Header = () => {
-  const dispatch = useDispatch()
-  const userLogin = useSelector(state => state.userLogin)
-  const { userInfo } = userLogin
+  const dispatch = useDispatch();
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
 
-  const logoutHandler =()=>{
-    dispatch(logout())
-  }
+  const logoutHandler = () => {
+    dispatch(logout());
+  };
 
   return (
     <header>
@@ -34,21 +33,36 @@ const Header = () => {
                   <LinkContainer to='/profile'>
                     <NavDropdown.Item>Profile</NavDropdown.Item>
                   </LinkContainer>
-                  <NavDropdown.Item onClick={logoutHandler}>Logout</NavDropdown.Item>
+                  <NavDropdown.Item onClick={logoutHandler}>
+                    Logout
+                  </NavDropdown.Item>
                 </NavDropdown>
-              ): (
-              <LinkContainer to='/login'>
-                <Nav.Link>
-                  <i className='fas fa-user'></i>Sign In
-                </Nav.Link>
-             </LinkContainer>)}
-              
+              ) : (
+                <LinkContainer to='/login'>
+                  <Nav.Link>
+                    <i className='fas fa-user'></i>Sign In
+                  </Nav.Link>
+                </LinkContainer>
+              )}
+              {userInfo && userInfo.isAdmin && (
+                <NavDropdown title='Admin' id='adminmenu'>
+                  <LinkContainer to='/admin/userlist'>
+                    <NavDropdown.Item>Users</NavDropdown.Item>
+                  </LinkContainer>
+                  <LinkContainer to='/admin/productlist'>
+                    <NavDropdown.Item>Products</NavDropdown.Item>
+                  </LinkContainer>
+                  <LinkContainer to='/admin/orderlist'>
+                    <NavDropdown.Item>Orders</NavDropdown.Item>
+                  </LinkContainer>
+                </NavDropdown>
+              )}
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,16 +1,16 @@
-import React, { useEffect, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { Col, Row } from 'react-bootstrap'
-import Product from '../components/Product'
-import Message from '../components/Message'
-import Loader from '../components/Loader'
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Col, Row } from 'react-bootstrap';
+import Product from '../components/Product';
+import Message from '../components/Message';
+import Loader from '../components/Loader';
 
-import { listProducts } from '../actions/productActions'
+import { listProducts } from '../actions/productActions';
 
 const HomeScreen = () => {
-  const dispatch = useDispatch()
-  const productList = useSelector((state) => state.productList)
-  const { loading, error, products } = productList
+  const dispatch = useDispatch();
+  const productList = useSelector((state) => state.productList);
+  const { loading, error, products } = productList;
 
   useEffect(() => {
     // const fetchProducts = async () => {
@@ -18,8 +18,8 @@ const HomeScreen = () => {
     //   setProducts(data)
     // }
     // fetchProducts()
-    dispatch(listProducts())
-  }, [dispatch])
+    dispatch(listProducts());
+  }, [dispatch]);
 
   return (
     <>
@@ -38,7 +38,7 @@ const HomeScreen = () => {
         </Row>
       )}
     </>
-  )
-}
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
