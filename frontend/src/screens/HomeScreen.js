c9fe6ecce6e17c475d8f6bcc6ccae52acc7a701d
Emre Ergül <eemreergul@gmail.com>
2020-10-24 14:53:36 +0300

    Product Search

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -7,7 +7,9 @@ import Loader from '../components/Loader';
 
 import { listProducts } from '../actions/productActions';
 
-const HomeScreen = () => {
+const HomeScreen = ({ match }) => {
+  const keyword = match.params.keyword;
+
   const dispatch = useDispatch();
   const productList = useSelector((state) => state.productList);
   const { loading, error, products } = productList;
@@ -18,8 +20,8 @@ const HomeScreen = () => {
     //   setProducts(data)
     // }
     // fetchProducts()
-    dispatch(listProducts());
-  }, [dispatch]);
+    dispatch(listProducts(keyword));
+  }, [dispatch, keyword]);
 
   return (
     <>
